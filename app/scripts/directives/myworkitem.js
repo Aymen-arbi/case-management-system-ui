4f5993d4aba59f701e2851ba87e69d7ac89e6e16
Aymen Arbi <aymen.arbi@gmail.com>
2015-10-06 11:29:11 +0200

    modified and fixed work item heading

--- app/scripts/directives/myworkitem.js
+++ app/scripts/directives/myworkitem.js
@@ -12,17 +12,17 @@ angular.module('caseManagementSystemUiApp')
 			template: '<div>' +
 				'<accordion close-others="oneAtATime">' +
 				'<accordion-group is-open="status.open" class="panel-danger workItem">' +
-				'<accordion-heading ng-controller = "WorkitemcontrollerCtrl">' +
+				'<accordion-heading>' +
 				'<div class="workItemInfo">Title !' +
 				'<i class="pull-right glyphicon" ng-class="{\'glyphicon-chevron-down\': status.open, \'glyphicon-chevron-right\': !status.open}"> </i>' +
-				'<progressbar class="progress-striped active workItemStatus" animate="false" value="dynamic" type="success">' +
-				' <b>{{dynamic}}%</b></progressbar>' +
-				'</div>' +
-				'<div class="workItemInfo">' +
-				'<p class="workItemName">Aymen Arbi</p>' +
 				'<p class="workItemPrio">Prio : Hight</p>' +
 				'</div>' +
 				'</accordion-heading>' +
+				'<div class="workItemInfo" ng-controller = "WorkitemcontrollerCtrl">' +
+				'<p class="workItemName">Aymen Arbi</p>' +
+				'<progressbar class="progress-striped active workItemStatus" animate="false" value="dynamic" type="success">' +
+				' <b>{{dynamic}}%</b></progressbar>' +
+				'</div>' +
 				'<h4>Descritpion:</h4>' +
 				'<p>This is just some content to illustrate fancy headings.</p>' +
 				'</accordion-group>' +
