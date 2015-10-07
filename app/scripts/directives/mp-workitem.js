bc8404ee27b7cacd7f36c50ae2668b735b645f1e
Marcus Svensson <svensson.marcus.9@gmail.com>
2015-10-07 10:59:46 +0200

    Moved workitem to own html file

--- app/scripts/directives/mp-workitem.js
+++ app/scripts/directives/mp-workitem.js
@@ -7,28 +7,17 @@
  * # myworkItem
  */
 angular.module('caseManagementSystemUiApp')
-	.directive('myWorkItem', function () {
+	.directive('mpWorkItem', function () {
 		return {
-			template: '<div>' +
-				'<accordion close-others="oneAtATime">' +
-				'<accordion-group is-open="status.open" class="panel-danger workItem">' +
-				'<accordion-heading>' +
-				'<div class="workItemInfo">Title !' +
-				'<i class="pull-right glyphicon" ng-class="{\'glyphicon-chevron-down\': status.open, \'glyphicon-chevron-right\': !status.open}"> </i>' +
-				'<p class="workItemPrio">Prio : Hight</p>' +
-				'</div>' +
-				'</accordion-heading>' +
-				'<div class="workItemInfo" ng-controller = "WorkitemcontrollerCtrl">' +
-				'<p class="workItemName">Aymen Arbi</p>' +
-				'<progressbar class="progress-striped active workItemStatus" animate="false" value="dynamic" type="success">' +
-				' <b>{{dynamic}}%</b></progressbar>' +
-				'</div>' +
-				'<h4>Descritpion:</h4>' +
-				'<p>This is just some content to illustrate fancy headings.</p>' +
-				'</accordion-group>' +
-				'</accordion>' +
-				'</div>',
-			restrict: 'E'
+			scope: {
+				//Todo story: '='
+			},
+			templateUrl: 'views/workitem.html',
+			restrict: 'E',
+			controller: function ($scope) {
+					$scope.story = {};
+					$scope.story.name = "Hejhej";
+				}
 				// link: function postLink(scope, element, attrs) {
 				//   element.text('this is the myworkItem directive');
 				// }
