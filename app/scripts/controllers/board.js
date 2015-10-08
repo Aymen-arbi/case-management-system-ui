28ad5193e0c299206109c9ac21e708fc78cf2d06
Marcus Svensson <svensson.marcus.9@gmail.com>
2015-10-08 20:20:41 +0200

    Started with drag and drop implementation

--- app/scripts/controllers/board.js
+++ app/scripts/controllers/board.js
@@ -2,16 +2,44 @@
 
 angular.module('caseManagementSystemUiApp')
 	.controller('BoardCtrl', function ($scope, $routeParams, boardService, socket) {
+		var projectId = $routeParams.id;
+		$scope.backlog = [];
+		$scope.inProgress = [];
+		$scope.test = [];
+		$scope.done = [];
+
+		function populateArrayStatus(array) {
+			for (var i = 0; i < array.length; i++) {
+				var status = array[i].status;
+
+				switch (status) {
+				case 'PENDING':
+					$scope.backlog.push(array[i]);
+					break;
+				case 'INPROGRESS':
+					$scope.inProgress.push(array[i]);
+					break;
+				case 'TEST':
+					$scope.test.push(array[i]);
+					break;
+				case 'DONE':
+					$scope.done.push(array[i]);
+					break;
+				default:
+					$scope.backlog.push(array[i]);
+				}
+			}
+		}
+
 		function getAll() {
 			boardService.getStories(projectId)
 				.then(function (res) {
 					$scope.stories = res.data;
+					populateArrayStatus($scope.stories);
 				}, function (res) {
 					console.log(res);
 				});
 		}
-
-		var projectId = $routeParams.id;
 
 		getAll();
 
@@ -28,4 +56,16 @@ angular.module('caseManagementSystemUiApp')
 		socket.on('update stories', function () {
 			getAll();
 		});
+
+		$scope.dragControlListeners = {
+			itemMoved: function (event) {
+				var moveSuccess;
+				console.log(event);
+
+				moveSuccess = function () {
+					console.log(event);
+				};
+				// socket.emit('update stories');
+			}
+		};
 	});
