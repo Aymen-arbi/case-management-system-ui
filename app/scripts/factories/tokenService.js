4b9580be13680150f897672a42cf3e71ceb7b325
Patrik Appelqvist <Appelqvist.patrik@gmail.com>
2015-10-14 14:28:28 +0200

    added https to boardservice and added a tokenStorage factory

---
+++ app/scripts/factories/tokenService.js
@@ -0,0 +1,17 @@
+'use strict';
+angular.module('caseManagementSystemUiApp')
+	.factory('tokenService', ['$window', function ($window) {
+		var tokenStorage = $window.localStorage;
+		var key = "token";
+		return {
+			persistToken: function (token) {
+				tokenStorage[key] = JSON.stringify(token);
+			},
+			getToken: function () {
+				return JSON.parse(key) || '""';
+			},
+			removeToken: function () {
+				tokenStorage.clear();
+			}
+		};
+	}]);
