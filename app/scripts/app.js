ad00a75f15534d9900fd6a9d6e7b2c3ee08e5b3a
Patrik Appelqvist <Appelqvist.patrik@gmail.com>
2015-10-15 13:16:06 +0200

    some changes

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -24,12 +24,13 @@ angular
     $routeProvider
       .when('/projects/:id', {
         templateUrl: 'views/board.html',
-        controller: 'BoardCtrl'
+        controller: 'BoardCtrl',
       })
       .when('/projects/:id/overview', {
         templateUrl: 'views/overview.html',
         controller: 'OverviewCtrl'
-      }).when('/login', {
+      })
+      .when('/login', {
         templateUrl: 'views/login.html',
         controller: 'LoginCtrl'
       })
