f6878632b6d27908fe2577cb6c94b5290d7f8b5e
Jimmy <jimmy_man_7@hotmail.com>
2015-10-15 14:32:09 +0200

    almost finshed with email validation. no battery

--- app/scripts/controllers/login.js
+++ app/scripts/controllers/login.js
@@ -3,13 +3,14 @@ angular.module('caseManagementSystemUiApp')
 	.controller('LoginCtrl', function ($scope, $location, boardService, authService, tokenService) {
 		var projectId = 'huryOspdl12';
 		$scope.user = {};
+		$scope.emailCheck = /\S+@\S+\.\S+/;
 		$scope.linksLeft = [{
 			link: 'Sign up',
 			click: function () {}
 		}];
 
 		$scope.login = function () {
-			if ($scope.user.email && $scope.user.password) {
+			if (!$scope.login.$error) {
 				authService.login($scope.user.email, $scope.user.password)
 					.then(function (res) {
 						tokenService.persistToken(res.headers('Authorization'));
